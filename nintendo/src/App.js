3023f5a13ca070afab80c8daa9f397b12907d035
Khalotis11 <moekhaloti@gmail.com>
2019-06-02 16:00:45 +0000

    Added Display component

--- nintendo/src/App.js
+++ nintendo/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { LeftConsole } from "./components/leftConsole/LeftConsole"
+import { LeftConsole } from "./components/leftConsole/LeftConsole";
+import { Display } from "./components/nintendoDisplay/Display";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LeftConsole />
+        <div className="nintendo-container">
+          {" "}
+          <LeftConsole />
+          <Display />
+        </div>
       </header>
     </div>
   );
